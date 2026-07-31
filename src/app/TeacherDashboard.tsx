b01a394c27cfef6144226fdceeb07638c241e185
Soptorshi Sarker Smriti <smritisarker003@gmail.com>
2026-07-31 20:44:21 +0600

    Convert Bangla into English in teacher Dashboard

---
+++ src/app/TeacherDashboard.tsx
@@ -0,0 +1,34 @@
+import TeacherPage, {
+  Student,
+  HomeworkSubmission,
+  NotificationItem,
+  Course,
+  ASSIGNED_COURSES,
+  INITIAL_STUDENTS,
+  INITIAL_HOMEWORKS,
+  INITIAL_NOTIFICATIONS,
+  dictionary,
+  getLocalizedText,
+  toBNDigits,
+  formatNumber,
+} from "./TeacherPage";
+
+export type {
+  Student,
+  HomeworkSubmission,
+  NotificationItem,
+  Course,
+};
+
+export {
+  ASSIGNED_COURSES,
+  INITIAL_STUDENTS,
+  INITIAL_HOMEWORKS,
+  INITIAL_NOTIFICATIONS,
+  dictionary,
+  getLocalizedText,
+  toBNDigits,
+  formatNumber,
+};
+
+export default TeacherPage;
